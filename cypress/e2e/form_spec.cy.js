22657a890e280d49896c22b55d96e0a24698e0cf
Gauri Joshi <gauri.joshi@planetargon.com>
2023-07-03 14:35:10 -0700

    Asserts empty values for form email and password

--- cypress/e2e/form_spec.cy.js
+++ cypress/e2e/form_spec.cy.js
@@ -6,8 +6,8 @@ describe('Feedback Loop form flow', () => {
   });
   it('Should have a form for user login', () => {
     cy.get('form').within(() => {
-      cy.get('input[type="Email"]')
-      cy.get('input[type="Password"]')
+      cy.get('input[type="Email"]').should('have.value', '')
+      cy.get('input[type="Password"]').should('have.value', '')
       cy.get('button').as('Login')
     });
   });
