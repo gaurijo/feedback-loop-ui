18c5508327e0845f6587af3cde101af664682a0b
Gauri Joshi <gauri.joshi@planetargon.com>
2023-07-04 20:16:03 -0700

    Test dashboard flow after user logs in

--- cypress/e2e/dashboard_spec.cy.js
+++ cypress/e2e/dashboard_spec.cy.js
@@ -1,6 +1,20 @@
 /* eslint-disable no-undef */
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Dashboard after Login Flow', () => {
+  beforeEach(() => {
+    cy.login();
   })
-})
+
+  it('should render the title', () => {
+    cy.contains('Feedback Loop');
+  });
+
+  it('should display teammates names', () => {
+    cy.contains('Scott Ertmer')
+    cy.contains('Travis Rollins');
+  });
+
+  it('should display the teammates feedback', () => {
+    cy.contains("Your feedback game is TOO strong.")
+    cy.contains("I appreciate your positive energy");
+  });
+});
